0c0d2abeb43d98ab17e1fb0d8a3abf9766f3d12f
Svetlana Lap <laptevas80@gmail.com>
2022-09-17 00:14:32 +0300

    add Задача 4

---
+++ Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿int a, b, c;
+Console.WriteLine("Введите три числа");
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+c = Convert.ToInt32(Console.ReadLine());
+if ((a>=b) & (a>=c)) {
+   Console.WriteLine("{0} наибольшее число", a); 
+}
+else if ((b>=a) & (b>=c)) {
+    Console.WriteLine("{0} наибольшее число", b);
+}
+else if ((c>=a) & (c>=b)) {
+    Console.WriteLine("{0} наибольшее число", c);
+}    
