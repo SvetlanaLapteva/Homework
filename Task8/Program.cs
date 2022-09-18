2000678e27ffabf72d7cb1e0813aeb7cafb7feb9
Светлана Лаптева <laptevas@yandex.ru>
2022-09-18 20:34:53 +0300

    Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,12 @@
+﻿int n, i;
+Console.Write("Введите число n: ");
+string s = Console.ReadLine();
+n = Convert.ToInt32(s);
+i = 1;
+while (i <= n)
+{
+  if (i % 2 == 0){
+    Console.WriteLine(i);
+  }
+  i++;
+}
